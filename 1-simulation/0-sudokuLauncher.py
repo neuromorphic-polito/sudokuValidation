00a185243cb7aa9c2ca1fdd6be982f3e0453192b
riccardopignari <riccardo.pignari@gmail.com>
2025-03-06 19:01:08 +0100

    Remove GeNN precision from laucher

--- 1-simulation/0-sudokuLauncher.py
+++ 1-simulation/0-sudokuLauncher.py
@@ -17,24 +17,6 @@ for d in difficulties:
                 print(f'Difficulty: {d} | Puzzle: {p} | Bug Fix: {b} | Enhanced {e}')
 
 
-##############################
-# ##### GeNN Precision ##### #
-##############################
-precision = ['float16', 'float', 'double']
-difficulties = ['easy', 'med', 'hard']
-puzzles = [1, 2, 3]
-bugFix = [1]
-enhanced = [1]
-
-for m in precision:
-    for d in difficulties:
-        for p in puzzles:
-            for b in bugFix:
-                for e in enhanced:
-                    os.system(f'python 1-sudokuGENN-Precision.py -d {d} -p {p} -t 300 -b {b} -e {e} -g 1 -m {m}')
-                    print(f'Precision: {m} | Difficulty: {d} | Puzzle: {p} | Bug Fix: {b} | Enhanced: {e}')
-
-
 #########################
 # ##### SpiNNaker ##### #
 #########################
